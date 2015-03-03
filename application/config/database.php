9844aa2a3d0d51f92418418f38f093c12ea3029b
rod <rod6588@gmail.com>
2015-03-03 11:26:31 +0200

    CodeIgniter config

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'campusbox';
+$db['default']['password'] = 'allveelaev';
+$db['default']['database'] = 'campusbox';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
