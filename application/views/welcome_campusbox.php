13add6cb4f9985eca13dabe95840475be1cc3e16
rod <rod6588@gmail.com>
2015-03-07 20:32:03 +0200

    Welcome page

---
+++ application/views/welcome_campusbox.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style/welcome.css">
+	<title>Welcome to Campusbox</title>
+</head>
+<body>
+
+    <img width="25%"  src="/images/logo.png" alt="Campusbox logo">
+
+</body>
+</html>
