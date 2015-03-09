269c2174010d35e72366a0f026d5d11481f50d98
rod <rod6588@gmail.com>
2015-03-09 13:49:48 +0200

    Install Bootstrap, home controller

--- application/views/welcome_campusbox.php
+++ application/views/welcome_campusbox.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style/welcome.css">
-	<title>Welcome to Campusbox</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/welcome.css">
+    <title>Welcome | Campusbox</title>
 </head>
-<body>
-
-    <img width="25%"  src="/images/logo.png" alt="Campusbox logo">
-
+<body>    
+    <img width="25%" src="/images/logo.png" alt="Campusbox logo">
 </body>
 </html>
