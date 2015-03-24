c7b698c1e28eac12a0510be9265468b0c35ff915
rod <rod6588@gmail.com>
2015-03-24 12:28:51 +0200

    Installed CodeIgniter 3.0rc3 and Bootstrap 3.3.4.

--- application/views/items/view.php
+++
@@ -1,9 +0,0 @@
-<!-- Jumbotron -->
-<div class="jumbotron">
-	<div class="container">
-		<h1><?php echo $database_item['title'] ?></h1>
-		<p><?php echo $database_item['description'] ?></p>
-        <p><a class="btn btn-primary btn-lg" href="/index.php/users/view/<?php echo $database_item['userid']; ?>" role="button">Contact seller</a></p>
-		<p><a class="btn btn-primary btn-lg" href="/index.php" role="button">Back to Campusbox</a></p>
-	</div>
-</div>
--- application/views/users/success.php
+++
@@ -1,8 +0,0 @@
-<!-- Jumbotron -->
-<div class="jumbotron">
-	<div class="container">
-		<h1>Success!</h1>
-		<p>Welcome to Campusbox!</p>
-		<p><a class="btn btn-primary btn-lg" href="/" role="button"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Back to Campusbox</a></p>
-	</div>
-</div>
