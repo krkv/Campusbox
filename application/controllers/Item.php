12592a392189d92a1015c9d2e729869289b5a599
rodion <rod6588@gmail.com>
2015-03-30 08:55:39 +0300

    Disabled login and signup for logged in users. Created better view for items listing.

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -7,6 +7,7 @@ class Item extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->helper('form');
+        $this->load->helper('text');
         $this->load->library('form_validation');
         $this->load->model('item_model');
     }
