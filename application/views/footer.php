3928afa09ecff0bdb9b6a8ae9695e41d8e290103
rodion <rod6588@gmail.com>
2015-04-05 23:31:26 +0300

    Moved date to footer, provided some context.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,10 +1,18 @@
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo base_url("js/bootstrap.js") ?>"></script>   
-    <script src="<?php echo base_url("js/buttondisabled.js") ?>"></script>
-    <script src="<?php echo base_url("js/clock.js") ?>"></script>
+<div class="container">
+    <p class="text-center">
+        &copy; Campusbox 2015.
+        Campusbox is under development at University of Tartu.
+        Current time in Tartu is <span id="hours">0</span>:<span id="minutes">0</span>:<span id="seconds">0</span>.
+    </p>
+</div>
 
-    </body>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url("js/bootstrap.js") ?>"></script>   
+<script src="<?php echo base_url("js/buttondisabled.js") ?>"></script>
+<script src="<?php echo base_url("js/clock.js") ?>"></script>
+
+</body>
 </html>
