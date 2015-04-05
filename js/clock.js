70f81286eda63c19c125f4d5ae57ff39ca7f89fb
Vilx <villu.siiroja@gmail.com>
2015-04-05 19:52:57 +0300

    AJAX clock

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(){
+     setInterval(ajaxcall, 1000);
+ });
+ function ajaxcall(){
+     $.ajax({
+         url: 'gettime.php',
+         success: function(data) {
+             data = data.split(':');
+             $('#hours').html(data[0]);
+             $('#minutes').html(data[1]);
+             $('#seconds').html(data[2]);
+         }
+     });
+ }
