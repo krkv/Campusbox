6b308edf05ab765ddfc08d40ee4ed53199b47010
rodion <rod6588@gmail.com>
2015-05-12 00:06:16 +0300

    Revert "Add item dummy thumbnail"
    This reverts commit 5203de9716ce8eccdf405e067465e9d36aa50780.

--- application/views/item/list.php
+++ application/views/item/list.php
@@ -10,7 +10,6 @@
 
                 <div class="col-xs-12 col-sm-6 col-md-4">
                     <div class="thumbnail">
-                        <img src="<?php echo base_url('images/item.png') ?>">
                         <div class="caption">
                             
                             <h3><?php echo word_wrap($item->title, 20) ?></h3>
